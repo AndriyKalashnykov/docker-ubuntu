916633d86941dabd734f5c363c10d9a13ed235ac
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-05-19 00:43:28 -0400

    fix: updated run
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- go/run.sh
+++ go/run.sh
@@ -2,4 +2,6 @@
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock -it --rm go-dev bash
 
+# docker push andriykalashnykov/ubuntu-golang:tagname
+
 # docker run -v /home/$USER/.ssh:/home/user/.ssh -it --rm go-dev bash
